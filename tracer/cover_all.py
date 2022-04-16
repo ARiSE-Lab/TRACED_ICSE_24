ce6a200526b0e1267757273b5101a646bd574d2d
bstee615 <benjaminjsteenhoek@gmail.com>
2022-04-16 15:57:13 -0500

    Add command line options, clean coverage outputs on failure and between inputs, add unit test

--- tracer/cover_all.py
+++ tracer/cover_all.py
@@ -12,13 +12,18 @@ from collections import defaultdict
 import sys
 import logging
 from multiprocessing import Pool
+from sklearn.utils import shuffle
 
-## params
-sample_problems = 5
-sample_from_each_lang = 50
-## params
+import argparse
 
-logging.basicConfig(level=logging.DEBUG if sample_problems <= 5 and sample_from_each_lang <= 5 else logging.INFO)
+parser = argparse.ArgumentParser()
+parser.add_argument("--sample_problems", type=int, help="number of problems to sample", default=4053)
+parser.add_argument("--sample_from_each_lang", type=int, help="number of submissions from each language to sample from each problem")
+parser.add_argument("--nproc", type=int, help="number of concurrent processes", default=1)
+parser.add_argument("--shuffle", action="store_true", help="whether to shuffle the data before sampling")
+args = parser.parse_args()
+
+logging.basicConfig(level=logging.DEBUG if (args.sample_problems <= 5 and args.sample_from_each_lang is not None and args.sample_from_each_lang <= 5) else logging.INFO)
 log = logging.getLogger()
 
 
@@ -29,10 +34,10 @@ filext_to_lang = {
 }
 lang_to_filext = {v: k for k, v in filext_to_lang.items()}
 
-base_dir = Path("../Project_CodeNet")
-input_dir = Path("../all_input_output")
+base_dir = Path("../../Project_CodeNet")
+input_dir = Path("../../all_input_output")
 metadata_dir = base_dir / "metadata"
-new_src_dir = Path("../Project_CodeNet_full2/Project_CodeNet/data")
+new_src_dir = Path("../../Project_CodeNet_full2/Project_CodeNet/data")
 out_dir = Path("instrumented_output")
 exe_dir = Path("instrumented_exes")
 out_metadata_dir = Path("instrumented_metadata")
@@ -60,7 +65,7 @@ def compile_coverage(src_filepath, exe_filepath):
         log.debug(f"compile {src_filepath} -> {exe_filepath} stderr: {proc.stderr}")
 
 def run_input(exe_filepath, infile, outfile=sys.stdout):
-    proc = subprocess.run(f"./{exe_filepath}", stdin=infile, stdout=outfile, stderr=subprocess.PIPE, text=True, timeout=10)
+    proc = subprocess.run(f"./{exe_filepath}", stdin=infile, stdout=outfile, stderr=subprocess.PIPE, text=True, timeout=3)
     if proc.stderr:
         log.debug(f"run {exe_filepath} stderr: {proc.stderr}")
     return proc
@@ -71,19 +76,42 @@ def run_gcov(src_filename, outfile=sys.stdout):
         log.debug(f"gcov {src_filename} stderr: {proc.stderr}")
     return proc
 
-def do_one(problem_num):
+def do_one(problem_num, write=True):
     problem_name = 'p' + str(problem_num).rjust(5, "0")
-    problem_csv = metadata_dir / (problem_name + '.csv')
+    out_metadata_file = out_metadata_dir / (problem_name + "_coverage.csv")
+    if out_metadata_file.exists():
+        log.debug(f"{problem_name} done")
+        df = pd.read_csv(str(out_metadata_file))
+        return list(df["outcome"])
 
-    df = pd.read_csv(str(problem_csv))
-    total = len(df)
-    df = df[df["language"].isin(enabled_languages)]
+    try:
+        problem_csv = metadata_dir / (problem_name + '.csv')
 
-    df = pd.concat((group.head(sample_from_each_lang) for lang, group in df.groupby("language"))) # NOTE: for debug, get 1 from each eligible language
-    df["outcome"] = "unknown"
-    log.info(f"covering {problem_name} {len(df)=}")
+        df = pd.read_csv(str(problem_csv))
+        total = len(df)
+        df = df[df["language"].isin(enabled_languages)]
+        if args.sample_from_each_lang:
+            groups = []
+            for lang, group in df.groupby("language"):
+                group_cut = group.head(min(args.sample_from_each_lang, len(group)))
+                if len(group_cut) > 0:
+                    groups.append(group_cut)
+            df = pd.concat(groups)
+        if args.shuffle:
+            df = shuffle(df, random_state=0)
+        df["outcome"] = "unknown"
+        log.info(f"covering {problem_name} {len(df)=}")
+    except Exception:
+        log.exception(f"error processing {problem_name}, skipping")
+        return [f"{problem_name}_error"]
 
     for i, row in df.iterrows():
+        def set_outcome(status):
+            if df.at[i, "outcome"] == "unknown":
+                df.at[i, "outcome"] = status
+            else:
+                df.at[i, "outcome"] = df.at[i, "outcome"] + "," + status
+
         try:
             src_filepath = new_src_dir / row["problem_id"] / row["language"] / (row["submission_id"] + '.' + lang_to_filext[row["language"]])
             log.debug(f"covering {src_filepath}")
@@ -95,13 +123,14 @@ def do_one(problem_num):
                 compile_coverage(src_filepath, exe_filepath)
             except subprocess.CalledProcessError as e:
                 log.debug(f"outcome: compile error\n{traceback.format_exc()}")
-                df.at[i, "outcome"] = "compile_error"
+                set_outcome("compile_error")
                 continue
 
             sub_out_dir = out_dir / row["problem_id"] / row["language"] / row["submission_id"]
             sub_out_dir.mkdir(exist_ok=True, parents=True)
 
             for input_filepath in sorted((input_dir / row["problem_id"]).glob("input_*.txt")):
+                clean_coverage(row["submission_id"], all_files=False)
                 def get_output_filepath(suffix):
                     return sub_out_dir / (input_filepath.stem + suffix)
 
@@ -115,11 +144,11 @@ def do_one(problem_num):
                         exitcodefile.write(str(proc.returncode))
                 except subprocess.CalledProcessError as e:
                     log.debug(f"outcome: run error\n{traceback.format_exc()}")
-                    df.at[i, "outcome"] = "run_error"
+                    set_outcome("run_error")
                     continue
                 except subprocess.TimeoutExpired as e:
                     log.debug(f"outcome: timeout\n{traceback.format_exc()}")
-                    df.at[i, "outcome"] = "timeout"
+                    set_outcome("timeout")
                     continue
                 
                 try:
@@ -127,7 +156,7 @@ def do_one(problem_num):
                         run_gcov(src_filepath.name, outfile=outfile)
                 except subprocess.CalledProcessError as e:
                     log.debug(f"outcome: gcov error\n{traceback.format_exc()}")
-                    df.at[i, "outcome"] = "gcov_error"
+                    set_outcome("gcov_error")
                     continue
                 gcov_filepath = Path(src_filepath.name + ".gcov")
                 if gcov_filepath.exists():
@@ -136,24 +165,24 @@ def do_one(problem_num):
             # move extra gcov files to another directory
             clean_coverage(row["submission_id"])
             log.debug(f"outcome: success")
-            df.at[i, "outcome"] = "success"
+            set_outcome("success")
         except Exception:
             log.exception("unhandled error")
-            df.at[i, "outcome"] = "error"
+            set_outcome("error")
+            clean_coverage(row["submission_id"])
 
-    df.to_csv(str(out_metadata_dir / (problem_name + "_coverage.csv")))
-    return list(df["outcome"])
+    if write:
+        df.to_csv(str(out_metadata_file))
 
-# problems = range(4053)
-problems = range(sample_problems)
-# problems = range(1)
+def test_do_one():
+    do_one(1000, write=False)
+
+problems = range(args.sample_problems)
 it = problems
-it = Pool(3).imap_unordered(do_one, it)
-# it = Parallel(n_jobs=3)(delayed(do_one)(p) for p in it)  # parallel
-# it = (do_one(p) for p in it)  # sequential
+if args.nproc > 1:
+    it = Pool(args.nproc).imap_unordered(do_one, it)
+else:
+    it = (do_one(p) for p in it)
 
-outcomes = defaultdict(int)
-for outcome_set in tqdm.tqdm(it, total=len(problems), desc="running coverage"):
-    for outcome in outcome_set:
-        outcomes[outcome] += 1
-print(json.dumps(outcomes, indent=2))
+for _ in tqdm.tqdm(it, total=len(problems), desc="running coverage"):
+    pass
