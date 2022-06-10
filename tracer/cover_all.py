40a9846ab5cba1cef5c38773a5f14b2ec500517c
bstee615 <benjaminjsteenhoek@gmail.com>
2022-06-10 09:09:22 -0500

    Process coverage in tmpdir

--- tracer/cover_all.py
+++ tracer/cover_all.py
@@ -13,37 +13,11 @@ import sys
 import logging
 from multiprocessing import Pool
 from sklearn.utils import shuffle
+import os
+import tarfile
+import tempfile
 
 import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--sample_problems", type=int, help="number of problems to sample", default=4053)
-parser.add_argument("--sample_from_each_lang", type=int, help="number of submissions from each language to sample from each problem")
-parser.add_argument("--nproc", type=int, help="number of concurrent processes", default=1)
-parser.add_argument("--shuffle", action="store_true", help="whether to shuffle the data before sampling")
-args = parser.parse_args()
-
-logging.basicConfig(level=logging.DEBUG if (args.sample_problems <= 5 and args.sample_from_each_lang is not None and args.sample_from_each_lang <= 5) else logging.INFO)
-log = logging.getLogger()
-
-
-filext_to_lang = {
-    "c": "C",
-    "cpp": "C++",
-    "java": "Java",
-}
-lang_to_filext = {v: k for k, v in filext_to_lang.items()}
-
-base_dir = Path("../../Project_CodeNet")
-input_dir = Path("../../all_input_output")
-metadata_dir = base_dir / "metadata"
-new_src_dir = Path("../../Project_CodeNet_full2/Project_CodeNet/data")
-out_dir = Path("instrumented_output")
-exe_dir = Path("instrumented_exes")
-out_metadata_dir = Path("instrumented_metadata")
-out_metadata_dir.mkdir(exist_ok=True, parents=True)
-
-enabled_languages = "C C++".split()
 
 def clean_coverage(submission_id, all_files=True):
     if all_files:
@@ -65,7 +39,7 @@ def compile_coverage(src_filepath, exe_filepath):
         log.debug(f"compile {src_filepath} -> {exe_filepath} stderr: {proc.stderr}")
 
 def run_input(exe_filepath, infile, outfile=sys.stdout):
-    proc = subprocess.run(f"./{exe_filepath}", stdin=infile, stdout=outfile, stderr=subprocess.PIPE, text=True, timeout=3)
+    proc = subprocess.run(f"{exe_filepath}", stdin=infile, stdout=outfile, stderr=subprocess.PIPE, text=True, timeout=3)
     if proc.stderr:
         log.debug(f"run {exe_filepath} stderr: {proc.stderr}")
     return proc
@@ -177,12 +151,61 @@ def do_one(problem_num, write=True):
 def test_do_one():
     do_one(1000, write=False)
 
-problems = range(args.sample_problems)
-it = problems
-if args.nproc > 1:
-    it = Pool(args.nproc).imap_unordered(do_one, it)
-else:
-    it = (do_one(p) for p in it)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sample_problems", type=int, help="number of problems to sample", default=4053)
+    parser.add_argument("--sample_from_each_lang", type=int, help="number of submissions from each language to sample from each problem")
+    parser.add_argument("--nproc", type=int, help="number of concurrent processes", default=1)
+    parser.add_argument("--shuffle", action="store_true", help="whether to shuffle the data before sampling")
+    parser.add_argument("--input_dir", type=Path, help="program input directory", default=Path("../all_input_output"))
+    parser.add_argument("--keep", action="store_true", help="whether to keep the temp dir after running")
+    args = parser.parse_args()
 
-for _ in tqdm.tqdm(it, total=len(problems), desc="running coverage"):
-    pass
+    logging.basicConfig(level=logging.DEBUG if (args.sample_problems <= 5 and args.sample_from_each_lang is not None and args.sample_from_each_lang <= 5) else logging.INFO)
+    log = logging.getLogger()
+
+    filext_to_lang = {
+        "c": "C",
+        "cpp": "C++",
+        "java": "Java",
+    }
+    lang_to_filext = {v: k for k, v in filext_to_lang.items()}
+
+    cur_dir = Path.cwd().absolute()
+    input_dir = args.input_dir.absolute()
+    base_dir = Path("../Project_CodeNet").absolute()
+    metadata_dir = (base_dir/"metadata")
+    new_src_dir = (base_dir/"data")
+
+    tmpdirname = tempfile.mkdtemp()
+    try:
+        tmp_dir = Path(tmpdirname)
+        print("running in", tmp_dir)
+        os.chdir(tmp_dir)
+        out_dir = tmp_dir/"instrumented_output"
+        exe_dir = tmp_dir/"instrumented_exes"
+        out_metadata_dir = tmp_dir/"instrumented_metadata"
+        out_dir.mkdir()
+        exe_dir.mkdir()
+        out_metadata_dir.mkdir()
+
+        enabled_languages = "C C++".split()
+
+        problems = range(args.sample_problems)
+        it = problems
+        if args.nproc > 1:
+            it = Pool(args.nproc).imap_unordered(do_one, it)
+        else:
+            it = (do_one(p) for p in it)
+
+        for _ in tqdm.tqdm(it, total=len(problems), desc="running coverage"):
+            pass
+
+        output_filename = cur_dir / "output.tar"
+        with tarfile.open(output_filename, "w") as tar:
+            tar.add(out_dir, arcname=out_dir.name)
+            tar.add(exe_dir, arcname=exe_dir.name)
+            tar.add(out_metadata_dir, arcname=out_metadata_dir.name)
+    finally:
+        if not args.keep:
+            shutil.rmtree(tmpdirname)
